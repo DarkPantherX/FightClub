ae3481074faa2123cc3812d77b00aed62fb1758c
DarkPantherX <Dark-Panther@gmx.ch>
2013-03-04 15:52:34 +0100

    Added bullets ArrayList and added the drawing for bullets

--- src/FightClub.java
+++ src/FightClub.java
@@ -4,7 +4,6 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -22,12 +21,14 @@ public class FightClub extends JPanel implements Runnable{
 
 
 	private static boolean paused=false;
+	
 
 	private Player player;
 	private InputHandler inHandler;
 
 	public static final int NUMKEYS=5;
-	public static List<Bullet> bullets= new ArrayList<Bullet>();
+	public static ArrayList<Bullet> bullets= new ArrayList<Bullet>();
+	public static double lasttime=0;
 	
 
 
@@ -43,20 +44,35 @@ public class FightClub extends JPanel implements Runnable{
 
 		
 		
-		
+		bullets = new ArrayList<Bullet>();
 		player = new Player(50,50,50,50, img,10);
 		inHandler = new InputHandler();
 		addKeyListener(inHandler);
+		
 		new Thread(this).start();
 	}
 	
 
 	
 	public void paintComponent(Graphics g){
+		
+		
 		Graphics2D g2 = (Graphics2D)g;
 
 		g2.clearRect(0, 0, 500, 500);
-		bullet.render(g2);
+
+		for(int w=0; w<bullets.size();w++){
+		System.out.println("this");
+		Bullet bul = (Bullet) bullets.get(w);
+		
+		if(bul.getX_Point()>500||bul.getX_Point()<0||bul.getY_Point()>500||bul.getY_Point()<0){
+			bullets.remove(w);
+		}else{
+			System.out.println("Works: "+w);
+		bul.render(g2);
+			}
+		}
+		System.out.println(System.currentTimeMillis());
 		player.render(g2);
 		
 	}
@@ -87,7 +103,7 @@ public class FightClub extends JPanel implements Runnable{
 
 	public void run() {
 		while (!paused) {
-			boolean a[]= new boolean[4];
+			boolean a[]= new boolean[NUMKEYS];
 			double b=0;
 			
 			if(inHandler.getKeys()[KeyEvent.VK_W]){
@@ -125,10 +141,9 @@ public class FightClub extends JPanel implements Runnable{
 			
 			player.update(a,b);
 			
-			for(int i=0;i<4;i++){
+			for(int i=0;i<NUMKEYS;i++){
 				a[i]=false;
 			}
-			System.out.println(player.getX_Point()+" "+ player.getY_Point());
 			repaint();
 
 			try {
@@ -139,6 +154,26 @@ public class FightClub extends JPanel implements Runnable{
 			}
 		}
 	}
+
+
+
+	public static ArrayList<Bullet> getBullets() {
+		return bullets;
+	}
+
+
+
+	public static double getLasttime() {
+		return lasttime;
+	}
+
+
+
+	public static void setLasttime(double lasttime) {
+		FightClub.lasttime = lasttime;
+	}
+
+
 
 
 	
