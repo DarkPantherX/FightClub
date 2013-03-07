9bdb9475d8c9cfe49fe7387039ad3fb27d886393
DarkPantherX <Dark-Panther@gmx.ch>
2013-03-07 14:27:15 +0100

    Added constructor argument for rotation of bullet type! Added Bullet Image Added Grads Added rotation logic Added bullet updates!

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,21 +1,30 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+
+import javax.swing.ImageIcon;
 
 
 public class Bullet extends GameComponents implements Renderer{
 
 
+	
+
 	public int dirX;
 	public int dirY;
+	public double grad;
+	private Image img= new ImageIcon(getClass().getResource("img/bullet.png")).getImage();
 	
-	
-	public Bullet(int width, int height, int dirX, int dirY,int x_Point, int y_Point){
+	public Bullet(int width, int height, int dirX, int dirY,int x_Point, int y_Point, double grad){
 		this.setWidth(width);
 		this.setHeight(height);
 		this.setDirX(dirX);
 		this.setDirY(dirY);
 		this.setX_Point(x_Point);
 		this.setY_Point(y_Point);
+		this.setGrad(grad);
 		
 		
 		
@@ -26,6 +35,18 @@ public class Bullet extends GameComponents implements Renderer{
 	
 	
 	
+		private void setGrad(double grad) {
+		this.grad=grad;
+		
+	}
+
+
+		private double getGrad() {
+			
+			return grad;
+		}
+
+
 		public int getDirX() {
 		return dirX;
 	}
@@ -46,12 +67,35 @@ public class Bullet extends GameComponents implements Renderer{
 
 
 	public void render(Graphics2D g) {
+		
+		
+		AffineTransform trans = new AffineTransform();
+	    trans.rotate(getGrad(),getX_Point()+getWidth() / 2, getY_Point()+getHeight() / 2);
+	    g.setTransform(trans);
+	    g.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.white);
-		g.fillRect(getX_Point(),getY_Point(),getWidth(),getHeight());
+		g.drawImage(getImg(), getX_Point(), getY_Point(), null);
 		
 	}
 
+	private Image getImg() {
+		
+		return img;
+	}
 
+
+
+
+
+	public void update(){
+		
+		setX_Point(getX_Point()+(getDirX()/20));
+		setY_Point(getY_Point()+(getDirY()/20));
+		
+		
+	}
 
 	
 }
