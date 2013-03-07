17e86fc15146ac7108184323029e472917b06c8e
DarkPantherX <Dark-Panther@gmx.ch>
2013-03-07 21:08:52 +0100

    Enemies are now implemented!

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,10 +1,11 @@
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 
 
 public class Enemy extends Entity implements Renderer{
 	
-	
+	Rectangle rect;
 	
 	
 	public Enemy(int x_Point,int  y_Point,int width,int height,Image img,int life){
@@ -14,19 +15,39 @@ public class Enemy extends Entity implements Renderer{
 		this.setHeight(height);
 		this.setImg(img);
 		this.setLife(life);
+		
+		rect = new Rectangle(getX_Point(),getY_Point(),30,30);
 	}
 
 	@Override
 	public void render(Graphics2D g) {
-		// TODO Auto-generated method stub
+		 g.drawImage(getImg(), getX_Point(), getY_Point(), null);
 		
 	}
 	
 	
 	public void update(){
+		Player player = FightClub.getNearestPlayer();
+		if(player.getX_Point()>getX_Point()){
+			setX_Point(getX_Point()+1);
+		}
 		
+		if(player.getX_Point()<getX_Point()){
+			setX_Point(getX_Point()-1);
+		}
+		if(player.getY_Point()<getY_Point()){
+			setY_Point(getY_Point()-1);
+		}
+		if(player.getY_Point()>getY_Point()){
+			setY_Point(getY_Point()+1);
+		}
+		rect = new Rectangle(getX_Point(),getY_Point(),30,30);
 		
-		
+	}
+
+	public Rectangle getRect() {
+		// TODO Auto-generated method stub
+		return rect;
 	}
 	
 
