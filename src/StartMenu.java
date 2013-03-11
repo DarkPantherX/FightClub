65836bd7d382d741d65efa10193a229449233db4
DarkPantherX <Dark-Panther@gmx.ch>
2013-03-11 01:02:46 +0100

    Made the startmenu look good

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -1,6 +1,10 @@
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 
@@ -10,10 +14,29 @@ public class StartMenu implements Menu{
 	private int select=0;
 	private FightClub fightclub;
 	private boolean[] a;
+	private Font registerFont;
+	private ArrayList<IntroEnemy> introenemies = new ArrayList<IntroEnemy>();
+	private ArrayList<IntroEnemy> introenemiess = new ArrayList<IntroEnemy>();
+	private Image title;
+	
+	public StartMenu(){
+		
+		title = new ImageIcon(getClass().getResource("img/title.png")).getImage();
+		try {
+				registerFont = Font.createFont(Font.TRUETYPE_FONT, getClass().getClassLoader().getResource("font/crac.ttf").openStream());
+			} catch (FontFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+	}
+	
 	
 	@Override
 	public void render(Graphics2D g2) {
-		Image title = new ImageIcon(getClass().getResource("img/title.png")).getImage();
+		
 		g2.setColor(Color.red);
 		g2.drawImage(title, 40,20,400+(FightClub.WIDTH-500),150+(FightClub.HEIGHT-500),null);
 		
@@ -24,14 +47,88 @@ public class StartMenu implements Menu{
 				msg="-->"+options[i];
 				
 			}
-		g2.drawString(msg,FightClub.WIDTH/2-130,(50*i+200+(FightClub.HEIGHT-500)));
+			
+			
+			g2.setColor(Color.red);
+			g2.setFont(registerFont.deriveFont(17f));
+		g2.drawString(msg,FightClub.WIDTH/2-130,(50*i+250+(FightClub.HEIGHT-500)));
+		g2.setColor(new Color(127,32,32));
+		g2.drawString(msg,FightClub.WIDTH/2-130+2,(50*i+250+(FightClub.HEIGHT-500)));
+		g2.setColor(new Color(64,34,35));
+		g2.drawString(msg,FightClub.WIDTH/2-130+4,(50*i+250+(FightClub.HEIGHT-500)));
+		int ou=(int)(Math.random()*4);
+		System.out.println(ou);
+		if(!introenemies.isEmpty()){
+			
+		if(introenemies.size()*30+25<FightClub.WIDTH&&((IntroEnemy)introenemies.get(introenemies.size()-1)).getX_Point()>50){
+			
+			
+			
+			IntroEnemy enim = new IntroEnemy(-50, FightClub.HEIGHT-120, 30, 30, ou, 0);
+			introenemies.add(enim);
+		}
+		}else{
+			
+			IntroEnemy enim = new IntroEnemy(-50, FightClub.HEIGHT-120, 30, 30, ou, 0);
+			introenemies.add(enim);
+		}
+		
+		for(int g=0;g<introenemies.size();g++){
+			IntroEnemy en= (IntroEnemy) introenemies.get(g);
+			
+			en.render(g2);
+			en.update();
+			if(en.getDir()==0){
+			en.setY_Point(FightClub.HEIGHT-120);
+			
+			if(en.getX_Point()>FightClub.WIDTH){
+				introenemies.remove(en);
+			}
+		}
+		}
+		
+		if(!introenemiess.isEmpty()){
+		if(introenemiess.size()*30+25<FightClub.WIDTH&&((IntroEnemy)introenemiess.get(introenemiess.size()-1)).getX_Point()<FightClub.WIDTH-50){
+			
+			IntroEnemy enim = new IntroEnemy(FightClub.WIDTH+50, 180+(FightClub.HEIGHT-500), 30, 30, ou, 1);
+			introenemiess.add(enim);
+		}
+		}else{
+			
+			IntroEnemy enim = new IntroEnemy(FightClub.WIDTH+50, 180+(FightClub.HEIGHT-500), 30, 30, ou, 1);
+			introenemiess.add(enim);
+		}
+		
+		for(int g=0;g<introenemiess.size();g++){
+			IntroEnemy en= (IntroEnemy) introenemiess.get(g);
+			
+			en.render(g2);
+			en.update();
+			
+			en.setY_Point(180+(FightClub.HEIGHT-500));
+			
+			if(en.getX_Point()<-20){
+				introenemiess.remove(en);
+			
+		}
 		}
 		
 		
 		
-		g2.drawString("Copyright (C) by TeamMongoAffenClan", FightClub.WIDTH/2-130,FightClub.HEIGHT-30);
+		
+		}
+		
+		g2.setFont(registerFont.deriveFont(12f));
+		g2.setColor(new Color(49,83,111));
+		g2.drawString("Copyright (C) by DarKCoder", 0,FightClub.HEIGHT-20);
 	}
 
+	
+	
+	
+	
+	
+	
 	@Override
 	public void update(FightClub fightclub, boolean[] a) {
 		this.fightclub=fightclub;
