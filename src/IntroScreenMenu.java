725e2230ede693982059564292d557c40acbe1dd
DarkPantherX <Dark-Panther@gmx.ch>
2013-03-12 20:41:32 +0100

    Added rendering-hints to make the letters smoother

--- src/IntroScreenMenu.java
+++ src/IntroScreenMenu.java
@@ -3,6 +3,7 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.io.IOException;
 
@@ -13,7 +14,8 @@ public class IntroScreenMenu implements Menu {
 	private int i=0;
 	private Font registerFont;
 	private Image intro;
-	
+	private Image intro2;
+	private int count=0;
 	
 	
 	public IntroScreenMenu(){
@@ -29,6 +31,7 @@ public class IntroScreenMenu implements Menu {
 		} 
 		
 		intro = Toolkit.getDefaultToolkit().getImage((getClass().getResource("img/intro.png")));
+		intro2 = Toolkit.getDefaultToolkit().getImage((getClass().getResource("img/intro2.png")));
 	}
 	
 	
@@ -38,6 +41,11 @@ public class IntroScreenMenu implements Menu {
 		
 		
 		
+		g2.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+		if(count<=0){
 		g2.setColor(Color.white);
 		g2.fillRect(0, 0, FightClub.WIDTH, FightClub.HEIGHT);
 		g2.drawImage(intro, FightClub.WIDTH/2-170,FightClub.HEIGHT/2-100,null);
@@ -49,7 +57,23 @@ public class IntroScreenMenu implements Menu {
 		g2.drawString("DarkCoder Production", FightClub.WIDTH/2-213, 50);
 		g2.setColor(Color.black);
 		g2.drawString("DarkCoder Production", FightClub.WIDTH/2-210, 50);
-	
+		}else{
+			
+			g2.setColor(Color.black);
+			g2.fillRect(0, 0, FightClub.WIDTH, FightClub.HEIGHT);
+			g2.drawImage(intro2, FightClub.WIDTH/2-170,FightClub.HEIGHT/2-100,null);
+
+			g2.setFont(registerFont.deriveFont(53f));
+			g2.setColor(new Color(190,105,27));
+			g2.drawString("DarkCoder Production", FightClub.WIDTH/2-216, 50);
+			g2.setColor(new Color(224,203,153));
+			g2.drawString("DarkCoder Production", FightClub.WIDTH/2-213, 50);
+			g2.setColor(Color.white);
+			g2.drawString("DarkCoder Production", FightClub.WIDTH/2-210, 50);
+			
+		}
+		
+		
 	}
 
 	@Override
@@ -67,7 +91,26 @@ public class IntroScreenMenu implements Menu {
 			
 		}
 		
+		if(currentTime>timeOld+2750&&currentTime<timeOld+3000){
+			
+			count=1;
+
+			
+		}
+		if(currentTime>timeOld+3000&&currentTime<timeOld+3100){
+			
+			count=0;
+		}
 		
+		if(currentTime>timeOld+3250&&currentTime<timeOld+3500){
+			
+			count=1;
+		}
+		
+		if(currentTime>timeOld+3600){
+			
+			count=0;
+		}
 
 	}
 
