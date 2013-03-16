d80428e43bb0374cb748616e167ca468ad8a9811
DarkPantherX <Dark-Panther@gmx.ch>
2013-03-16 19:35:46 +0100

    Added "Power" to bullets Rearanged the class

--- src/Bullet.java
+++ src/Bullet.java
@@ -18,11 +18,12 @@ public class Bullet extends GameComponents implements Renderer{
 	public double dirX;
 	public double dirY;
 	public double grad;
+	private int power;
 	private Image img=  Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/bullet.png"));
 	private Point point;
 	private Rectangle rect;
 	
-	public Bullet(int width, int height, double dirX, double dirY,int x_Point, int y_Point, double grad){
+	public Bullet(int width, int height, double dirX, double dirY,int x_Point, int y_Point, double grad, int power){
 		this.setWidth(width);
 		this.setHeight(height);
 		this.setDirX(dirX);
@@ -30,6 +31,7 @@ public class Bullet extends GameComponents implements Renderer{
 		this.setX_Point(x_Point);
 		this.setY_Point(y_Point);
 		this.setGrad(grad);
+		this.setPower(power);
 		
 		
 		
@@ -38,8 +40,36 @@ public class Bullet extends GameComponents implements Renderer{
 	
 
 	
+public void render(Graphics2D g) {
+		
+		
+		
+		AffineTransform trans = new AffineTransform();
+	    trans.rotate(getGrad(),getX_Point()+getWidth() / 2, getY_Point()+getHeight() / 2);
+	    g.setTransform(trans);
+	    g.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+		g.drawImage(getImg(), (int)getX_Point(), (int)getY_Point(), null);
+		
+	}
 	
-	
+
+
+
+		public void update(){
+			
+			double velx=((FightClub.WIDTH/(double)500))*5;
+			double vely=((FightClub.HEIGHT/(double)500))*5;
+
+			setX_Point(getX_Point()+((getDirX()/20)*velx));
+			setY_Point(getY_Point()+((getDirY()/20)*vely));
+			setPoint(new Point((int)(getX_Point()+5),(int)(getY_Point()+5)));
+			rect = new Rectangle((int)getX_Point(),(int)getY_Point(),30,30);
+			
+		}
+
+
 		private void setGrad(double grad) {
 		this.grad=grad;
 		
@@ -71,19 +101,7 @@ public class Bullet extends GameComponents implements Renderer{
 
 
 
-	public void render(Graphics2D g) {
-		
-		
-		
-		AffineTransform trans = new AffineTransform();
-	    trans.rotate(getGrad(),getX_Point()+getWidth() / 2, getY_Point()+getHeight() / 2);
-	    g.setTransform(trans);
-	    g.setRenderingHint(
-                RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-		g.drawImage(getImg(), (int)getX_Point(), (int)getY_Point(), null);
-		
-	}
+	
 
 	private Image getImg() {
 		
@@ -94,17 +112,7 @@ public class Bullet extends GameComponents implements Renderer{
 
 
 
-	public void update(){
-		
-		double velx=((FightClub.WIDTH/(double)500))*5;
-		double vely=((FightClub.HEIGHT/(double)500))*5;
 
-		setX_Point(getX_Point()+((getDirX()/20)*velx));
-		setY_Point(getY_Point()+((getDirY()/20)*vely));
-		setPoint(new Point((int)(getX_Point()+5),(int)(getY_Point()+5)));
-		rect = new Rectangle((int)getX_Point(),(int)getY_Point(),30,30);
-		
-	}
 
 
 
@@ -133,8 +141,20 @@ public class Bullet extends GameComponents implements Renderer{
 
 
 	public Rectangle getRect() {
-		// TODO Auto-generated method stub
 		return rect;
+	}
+
+
+	//new introduced to the family: Bullet power!
+	
+	private void setPower(int power2) {
+	this.power=power2;
+	
+}
+
+
+	public int getPower() {
+		return power;
 	}
 
 	
