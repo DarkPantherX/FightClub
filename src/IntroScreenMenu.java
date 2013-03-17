317666f7f382f3ddd671838ab1d91afae13a37e4
Strichcoder <raphael.api@bluewin.ch>
2013-03-17 22:13:08 +0100

    Included the possibility to clip Intro by pressing ENTER.

--- src/IntroScreenMenu.java
+++ src/IntroScreenMenu.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
@@ -8,114 +7,102 @@ import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.io.IOException;
 
-
 public class IntroScreenMenu implements Menu {
 
 	private long timeOld;
-	private int i=0;
+	private int i = 0;
 	private Font registerFont;
 	private Image intro;
 	private Image intro2;
-	private int count=0;
+	private int count = 0;
+	private boolean [] a;
+
 	
-	
-	public IntroScreenMenu(){
-		
+	public IntroScreenMenu() {
+
 		try {
-			registerFont = Font.createFont(Font.TRUETYPE_FONT, getClass().getClassLoader().getResource("font/narrow.ttf").openStream());
+			registerFont = Font.createFont(Font.TRUETYPE_FONT, getClass()
+					.getClassLoader().getResource("font/narrow.ttf")
+					.openStream());
 		} catch (FontFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-		
-		intro = Toolkit.getDefaultToolkit().getImage((getClass().getResource("img/intro.png")));
-		intro2 = Toolkit.getDefaultToolkit().getImage((getClass().getResource("img/intro2.png")));
+		}
+
+		intro = Toolkit.getDefaultToolkit().getImage(
+				(getClass().getResource("img/intro.png")));
+		intro2 = Toolkit.getDefaultToolkit().getImage(
+				(getClass().getResource("img/intro2.png")));
 	}
-	
-	
-	
+
 	@Override
 	public void render(Graphics2D g2) {
-		
-		
-		
-		g2.setRenderingHint(
-                RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
 
-		if(count<=0){
-		g2.setColor(Color.white);
-		g2.fillRect(0, 0, FightClub.WIDTH, FightClub.HEIGHT);
-		g2.drawImage(intro, FightClub.WIDTH/2-170,FightClub.HEIGHT/2-100,null);
-		
-		
-		g2.setFont(registerFont.deriveFont(53f));
-		g2.setColor(new Color(65,150,228));
-		g2.drawString("DarkCoder Production", FightClub.WIDTH/2-216, 50);
-		g2.setColor(new Color(31,52,102));
-		g2.drawString("DarkCoder Production", FightClub.WIDTH/2-213, 50);
-		g2.setColor(Color.black);
-		g2.drawString("DarkCoder Production", FightClub.WIDTH/2-210, 50);
-		}else{
-			
-			g2.setColor(Color.black);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+
+		if (count <= 0) {
+			g2.setColor(Color.white);
 			g2.fillRect(0, 0, FightClub.WIDTH, FightClub.HEIGHT);
-			g2.drawImage(intro2, FightClub.WIDTH/2-170,FightClub.HEIGHT/2-100,null);
+			g2.drawImage(intro, FightClub.WIDTH / 2 - 170,
+					FightClub.HEIGHT / 2 - 100, null);
 
 			g2.setFont(registerFont.deriveFont(53f));
-			g2.setColor(new Color(190,105,27));
-			g2.drawString("DarkCoder Production", FightClub.WIDTH/2-216, 50);
-			g2.setColor(new Color(224,203,153));
-			g2.drawString("DarkCoder Production", FightClub.WIDTH/2-213, 50);
+			g2.setColor(new Color(65, 150, 228));
+			g2.drawString("DarkCoder Production", FightClub.WIDTH / 2 - 216, 50);
+			g2.setColor(new Color(31, 52, 102));
+			g2.drawString("DarkCoder Production", FightClub.WIDTH / 2 - 213, 50);
+			g2.setColor(Color.black);
+			g2.drawString("DarkCoder Production", FightClub.WIDTH / 2 - 210, 50);
+		} else {
+
+			g2.setColor(Color.black);
+			g2.fillRect(0, 0, FightClub.WIDTH, FightClub.HEIGHT);
+			g2.drawImage(intro2, FightClub.WIDTH / 2 - 170,
+					FightClub.HEIGHT / 2 - 100, null);
+
+			g2.setFont(registerFont.deriveFont(53f));
+			g2.setColor(new Color(190, 105, 27));
+			g2.drawString("DarkCoder Production", FightClub.WIDTH / 2 - 216, 50);
+			g2.setColor(new Color(224, 203, 153));
+			g2.drawString("DarkCoder Production", FightClub.WIDTH / 2 - 213, 50);
 			g2.setColor(Color.white);
-			g2.drawString("DarkCoder Production", FightClub.WIDTH/2-210, 50);
-			
+			g2.drawString("DarkCoder Production", FightClub.WIDTH / 2 - 210, 50);
+
 		}
-		
-		
+
 	}
 
 	@Override
 	public void update(FightClub fightclub, boolean[] a) {
-		long currentTime=System.currentTimeMillis();
+		long currentTime = System.currentTimeMillis();
+		this.a = a;
 		
-		if(i==0){
-			timeOld=currentTime;
+		if (i == 0) {
+			timeOld = currentTime;
 			i++;
-			
 		}
 		
-		if(currentTime>timeOld+5000){
+		if (currentTime > timeOld + 5000 || a[5]) { // a[5] = ENTER
 			fightclub.setMenu(new StartMenu());
-			
-			
+		}
+		if (currentTime > timeOld + 2750 && currentTime < timeOld + 3000) {
+			count = 1;
+		}
+		if (currentTime > timeOld + 3000 && currentTime < timeOld + 3100) {
+			count = 0;
+		}
+		if (currentTime > timeOld + 3250 && currentTime < timeOld + 3500) {
+			count = 1;
+		}
+		if (currentTime > timeOld + 3600) {
+			count = 0;
 		}
 		
-		if(currentTime>timeOld+2750&&currentTime<timeOld+3000){
-			
-			count=1;
-
-			
-		}
-		if(currentTime>timeOld+3000&&currentTime<timeOld+3100){
-			
-			count=0;
-		}
-		
-		if(currentTime>timeOld+3250&&currentTime<timeOld+3500){
-			
-			count=1;
-		}
-		
-		if(currentTime>timeOld+3600){
-			
-			count=0;
-		}
 
 	}
 
 }
-
