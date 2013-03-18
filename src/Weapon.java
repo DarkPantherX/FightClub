1c24bd4cf2e64aed568c8c1ced652fd2ce8db0cd
DarkPantherX <Dark-Panther@gmx.ch>
2013-03-18 14:15:42 +0100

    Initial commit

---
+++ src/Weapon.java
@@ -0,0 +1,45 @@
+
+public class Weapon {
+	
+	private String weaponName;
+	private int MAX_AMMO;
+	private int power;
+	/**
+	 * @return the weaponName
+	 */
+	public String getWeaponName() {
+		return weaponName;
+	}
+	/**
+	 * @param weaponName the weaponName to set
+	 */
+	public void setWeaponName(String weaponName) {
+		this.weaponName = weaponName;
+	}
+	/**
+	 * @return the mAX_AMMO
+	 */
+	public int getMAX_AMMO() {
+		return MAX_AMMO;
+	}
+	/**
+	 * @param mAX_AMMO the mAX_AMMO to set
+	 */
+	public void setMAX_AMMO(int mAX_AMMO) {
+		MAX_AMMO = mAX_AMMO;
+	}
+	/**
+	 * @return the power
+	 */
+	public int getPower() {
+		return power;
+	}
+	/**
+	 * @param power the power to set
+	 */
+	public void setPower(int power) {
+		this.power = power;
+	}
+	
+	
+}
