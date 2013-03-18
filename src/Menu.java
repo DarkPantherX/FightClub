1d88bca1e30c10132535032ac7bebee1874522f4
Strichcoder <raphael.api@bluewin.ch>
2013-03-19 00:13:30 +0100

    Wrote down ideas about the organisation of the Menus. - See "Menu.java" - Detected a Bug: Retry in LooseMenu.java does not set up corectly. As soon as one tries to shoot, there is a NullPointerException at Player.update

--- src/Menu.java
+++ src/Menu.java
@@ -6,4 +6,42 @@ public interface Menu {
 	public void render(Graphics2D g2);
 	public void update(FightClub fightclub, boolean[] a);
 	
+	/*
+	 * Short overview of the menus:
+	 * 
+	 * - IntroScreenMenu.java:
+	 * Intro with DarkCoder
+	 * 
+	 * - StartMenu.java:
+	 * Main menu with "Links to"
+	 * > Singleplayer
+	 * > Multiplayer // TODO longterm
+	 * > About // TODO AboutMenu.java
+	 * > Quit // TODO
+	 * 
+	 * - PauseMenu.java:
+	 * Pause can be summoned with Esc during the Singleplayergame
+	 * With "Links to"
+	 * > Retry // TODO
+	 * > Resume // "Done" with repressing Esc
+	 * > Main menu // TODO
+	 * 
+	 * - LooseMenu.java
+	 * Displayed when live of player <= 0
+	 * With "Links to"
+	 * > Retry 
+	 * > Main menu
+	 * > Quit
+	 * 
+	 * TODO
+	 * - WinMenu.java
+	 * 
+	 * TODO
+	 * - AboutMenu.java
+	 * With "Link to"
+	 * > Main menu // TODO
+	 * 
+	 */
+	
+	
 }
