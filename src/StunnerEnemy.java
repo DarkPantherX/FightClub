7c018390d3277b23dffb2ccd76cbca1f08edf34a
DarkPantherX <Dark-Panther@gmx.ch>
2013-07-10 18:40:34 +0200

    Added StunnerEnemy

---
+++ src/StunnerEnemy.java
@@ -0,0 +1,12 @@
+import java.awt.Image;
+
+
+public class StunnerEnemy extends Enemy{
+
+	public StunnerEnemy(int x_Point, int y_Point, int width, int height,
+			Image img, int life, boolean visible) {
+		super(x_Point, y_Point, width, height, img, life, visible);
+		
+	}
+
+}
