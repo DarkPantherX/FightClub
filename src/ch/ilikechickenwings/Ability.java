60b365bb97ea535015c90273104875363dc6fd92
DarkPantherX <Dark-Panther@gmx.ch>
2013-10-08 18:45:50 +0200

    interduced abilities

---
+++ src/ch/ilikechickenwings/Ability.java
@@ -0,0 +1,13 @@
+package ch.ilikechickenwings;
+
+import java.awt.Graphics2D;
+
+public class Ability extends GameComponents implements Renderer{
+
+	@Override
+	public void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
